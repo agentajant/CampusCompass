ea64ccd7f00db74a50f36c511272fb47dd57ae47
Ajant_Surve <agentajant@gmail.com>
2025-11-14 20:22:06 +0530

    main2

--- Assets/Scripts/Image_Recognizer.cs
+++ Assets/Scripts/Image_Recognizer.cs
@@ -10,6 +10,8 @@ public class Image_Recognizer : MonoBehaviour
     private ARTrackedImageManager arTrackedImageManager;
     public ARSession session;
     public PathFinder find;
+    public scene_changing_to_Selector change;
+    private string final_node = null;
     // Start is called before the first frame update
     void Awake()
     {
@@ -19,7 +21,7 @@ public class Image_Recognizer : MonoBehaviour
     {
         if (arTrackedImageManager.trackables.count > 1)
         {
-            Debug.Log("More than one image detected");
+            // Debug.Log("More than one image detected");
             session.Reset();
         }
     }
@@ -35,14 +37,22 @@ public class Image_Recognizer : MonoBehaviour
     {
         foreach (ARTrackedImage trackedImage in args.added)
         {
+            // if (final_node != null && trackedImage.referenceImage.name[0] == final_node[0])
+            // {
+            //     Debug.Log("You have reached your destination: ");
+            //     change.OnClick();
+            // }
+
             if (trackedImage.referenceImage.name[0] == PlayerPrefs.GetString("node1")[0])
             {
-                Debug.Log("Last Node " + PlayerPrefs.GetString("node2")[0]);
+                final_node = PlayerPrefs.GetString("node2");
+                Debug.Log("Last Node " + final_node);
             }
 
             else if(trackedImage.referenceImage.name[0] == PlayerPrefs.GetString("node2")[0])
             {
-                Debug.Log("Last Node " + PlayerPrefs.GetString("node1")[0]);
+                final_node = PlayerPrefs.GetString("node1");
+                Debug.Log("Last Node " + final_node);
             }
 
             else if (find.distance(trackedImage.referenceImage.name[0], PlayerPrefs.GetString("node1")[0]) <= find.distance(trackedImage.referenceImage.name[0], PlayerPrefs.GetString("node2")[0]))
@@ -57,10 +67,10 @@ public class Image_Recognizer : MonoBehaviour
             }
             
         }
-        foreach (ARTrackedImage trackedImage in args.removed)
-        {
-            Debug.Log("Removed Image: " + trackedImage.referenceImage.name);
-        }
+        // foreach (ARTrackedImage trackedImage in args.removed)
+        // {
+        //     Debug.Log("Removed Image: " + trackedImage.referenceImage.name);
+        // }
     }
 
     public void remove_objs()
